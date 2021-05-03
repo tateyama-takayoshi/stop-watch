f79b3d0b9a9da35e75ee599883be8635353cb6de
tateyama-takayoshi <teach.heme@gmail.com>
2021-05-03 22:52:09 +0000

    質問

---
+++ フィルター.js
@@ -0,0 +1,23 @@
+$(function() {
+ //変数
+let searchText; //入力文字列;
+let targetText; //リストのテキスト
+
+$(document).on("input", "#search-tex", function () {
+  searchText = $(this).val(); //入力文字取得
+
+  if (searchText !== "") {
+    //入力があった場合
+    $(".target-area li").each(function () {
+      //リスト毎に行う
+      targetText = $(this).text(); //リストのテキストを取得
+      if (targetText.indexOf(searchText) !== -1) {
+        //リストに入力された文字列が存在する場合
+        $(this).removeClass("hidden");
+      } else {
+        $(this).addClass("hidden");
+      } //if indexOz
+    }); //jq each
+  } //if serchText
+}); //jq documen
+});
